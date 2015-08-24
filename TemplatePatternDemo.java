d955d71ec96a3db8d865494d08bc65148eb823c6
李浩 <gzlihao2@corp.netease.com>
2015-08-24 14:32:21 +0800

    add java files

---
+++ TemplatePatternDemo.java
@@ -0,0 +1,12 @@
+package templatemethod;
+
+public class TemplatePatternDemo {
+	   public static void main(String[] args) {
+
+	      Game game = new Basketball ();
+	      game.play();
+	      System.out.println();
+	      game = new Football();
+	      game.play();      
+	   }
+	}
